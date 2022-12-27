2e242bd8e84b606b6b67a3281ca067fa675e9202
MS.vijay <115245339+Killervj03@users.noreply.github.com>
2022-12-27 20:17:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComp from './Components/Navbar Components';
+
+import Home from './Components/Home';
+import OurServices from './Components/ourservices';
+import Ourteam from './Components/ourteam';
+import Trainingconsulting from './Components/Training-consulting';
+import Aboutus from './Components/About us';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <NavbarComp />
+      <Home />
+      < OurServices />
+      <Ourteam />
+      <Trainingconsulting />
+      <Aboutus />
+
+
+      <a href="https:/wa.me/+917200479030" target="_blank" class="whatsapp_float"><i class="fa-brands fa-whatsapp whatsapp-icon"></i></a>
+   
     </div>
   );
 }
